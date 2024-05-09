75e00e4f38b5048f89a5951e478a58bc5a4e6b13
Mr. Stone <pierrevensy@gmail.com>
2024-05-09 11:37:32 -0400

    feat: initial release with full SSR/SPA React integration (#1)

---
+++ src/commands/InitCommand.ts
@@ -0,0 +1,56 @@
+import { Argv } from 'yargs'
+import { IncomingEvent } from '@stone-js/core'
+import { ConsoleContext } from '../declarations'
+import { AppBuilder } from '../create/AppBuilder'
+import { CommandOptions } from '@stone-js/node-cli-adapter'
+
+/**
+ * The init command options.
+ */
+export const initCommandOptions: CommandOptions = {
+  name: 'init',
+  alias: 'i',
+  args: ['[project-name]'],
+  desc: 'Create a fresh Stone app from a starter template',
+  options: (yargs: Argv) => {
+    return yargs
+      .positional('project-name', {
+        type: 'string',
+        desc: 'your project name'
+      })
+      .option('yes', {
+        alias: 'y',
+        default: false,
+        type: 'boolean',
+        desc: 'create with default values'
+      })
+      .option('force', {
+        alias: 'f',
+        type: 'boolean',
+        desc: 'Force overriding'
+      })
+  }
+}
+
+/**
+ * The init command class.
+ */
+export class InitCommand {
+  /**
+   * Create a new instance of CoreServiceProvider.
+   *
+   * @param context - The service container to manage dependencies.
+   */
+  constructor (private readonly context: ConsoleContext) {}
+
+  /**
+   * Handle the incoming event.
+   */
+  async handle (event: IncomingEvent): Promise<void> {
+    try {
+      await new AppBuilder(this.context).build(event)
+    } catch (error: any) {
+      this.context.commandOutput.error(error.message)
+    }
+  }
+}
---
+++ src/server/rollup-config.ts
@@ -0,0 +1,77 @@
+import json from '@rollup/plugin-json'
+import babel from '@rollup/plugin-babel'
+import multi from '@rollup/plugin-multi-entry'
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import nodeExternals from 'rollup-plugin-node-externals'
+import { defineConfig, RollupLog, LoggingFunction } from 'rollup'
+
+/**
+ * Generate Rollup build options for the entire application.
+*/
+const rollupBuildConfig = defineConfig({
+  input: 'app/**/*.ts',
+  context: 'globalThis',
+  output: {
+    format: 'es',
+    file: 'dist/app.mjs'
+  },
+  plugins: [
+    multi(),
+    nodeExternals(), // Must always be before `nodeResolve()`.
+    nodeResolve({
+      extensions: ['.js', '.mjs', '.ts', '.json'],
+      exportConditions: ['node', 'import', 'require', 'default']
+    }),
+    json(),
+    commonjs({ include: /node_modules/, transformMixedEsModules: true }),
+    babel({
+      babelrc: false,
+      configFile: false,
+      babelHelpers: 'bundled',
+      extensions: ['.js', '.mjs', '.ts'],
+      presets: [
+        ['@babel/preset-env', {
+          targets: { node: '20' },
+          bugfixes: true,
+          modules: false,
+          useBuiltIns: false
+        }],
+        '@babel/preset-typescript'
+      ],
+      plugins: [['@babel/plugin-proposal-decorators', { version: '2023-11' }]]
+    })
+  ]
+})
+
+/**
+ * Generate Rollup bundle options for the entire application.
+*/
+const rollupBundleConfig = defineConfig({
+  input: 'app/**/*.ts',
+  context: 'globalThis',
+  output: {
+    format: 'es',
+    file: 'dist/app.mjs'
+  },
+  plugins: [
+    nodeExternals({ deps: false }), // Must always be before `nodeResolve()`.
+    nodeResolve({
+      extensions: ['.js', '.mjs', '.ts', '.json'],
+      exportConditions: ['node', 'import', 'require', 'default']
+    }),
+    json(),
+    commonjs({ include: /node_modules/, transformMixedEsModules: true })
+  ],
+  onwarn (warning: RollupLog, warn: LoggingFunction) {
+    // Suppress only circular dependency warning
+    if (
+      warning.code === 'CIRCULAR_DEPENDENCY' &&
+      /node_modules[/\\]/.test(warning.message)
+    ) { return }
+
+    warn(warning)
+  }
+})
+
+export { rollupBuildConfig, rollupBundleConfig }
